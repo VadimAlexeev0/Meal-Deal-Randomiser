3d094ba13f0a601e6944b73bec3b5184a53e7604
UncleBen69 <valexeev30@gmail.com>
2020-02-03 12:32:22 +0000

    BoilerPlate

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Layout from "../components/layout";
+
+class Index extends React.Component {
+	render() {
+		return (
+			<Layout>
+				<h1>Index</h1>
+			</Layout>
+		);
+	}
+}
+
+export default Index;
