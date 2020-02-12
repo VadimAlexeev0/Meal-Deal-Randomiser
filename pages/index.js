46f2c321345d6a4cea6a07ed923d7ebdbd0b089e
UncleBen69 <valexeev30@gmail.com>
2020-02-12 09:46:18 +0000

    Convert to Static Generation

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,15 @@
 import Layout from "../components/layout";
-import Randomiser from "../components/randomiser";
 import Columns from "../components/columns/columns"
 
 
-import fetch from 'isomorphic-unfetch'
-
 class Index extends React.Component {
-	static async getInitialProps(ctx) {
-		const res = await fetch('http://localhost:3000/api/v0/getdata/tesco')
-		const json = await res.json()
-		return { data: json }
-	}
-
-
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			data: this.props.data,
+			data: require("../tescoData.json"),
 		};
 	}
-
 	render() {
 		return (
 			<Layout>
